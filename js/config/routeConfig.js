b07f709c03a51e4711520e4c373f48ecb9b5b022
Marcelo Okazaki <marcelo.okazaki@fcamara.com.br>
2022-01-15 12:16:59 -0300

    Alteracao no controller

--- js/config/routeConfig.js
+++ js/config/routeConfig.js
@@ -2,20 +2,10 @@ angular.module("app").config(function ($routeProvider) {
 	$routeProvider.when("/list", {
 		templateUrl: "view/list.html",
 		controller: "listController",
-		// resolve: {
-		// 	list: function (appService) {
-		// 		return appService.getAll();
-		// 	},
-		// }
 	});
 	$routeProvider.when("/details/:id", {
 		templateUrl: "view/details.html",
 		controller: "detailsController",
-		// resolve: {
-		// 	contato: function (contatosAPI, $route) {
-		// 		return contatosAPI.getContato($route.current.params.id);
-		// 	}
-		// }
 	});
 	$routeProvider.otherwise({redirectTo: "/list"});
 });
